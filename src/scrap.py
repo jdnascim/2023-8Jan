6373c843fb164933b7eca7cf1ec99973ba6c1a0a
José Nascimento <jose.dori.nascimento@gmail.com>
2023-01-13 01:42:52 -0300

    more modularization

--- src/scrap.py
+++ src/scrap.py
@@ -1,15 +1,34 @@
 from utils import get_client
 
-async def get_groups(client):
+async def list_dialogs(client):
+    """ list all dialogs you are part of (name and id)
+
+    Args:
+        client (object): telethon client object
+    """
     async for dialog in client.iter_dialogs():
         print(dialog.name, 'has ID', dialog.id)
+
+
+async def scrap_dialog(client, dialog_id, media=True):
+    """ get message from a given 
+
+    Args:
+        client (object): telethon client object
+        dialog_id (int): id of the dialog to be scraped
+        media (bool, optional): If it will download the media attached to 
+                                the messages. Defaults to True.
+    """
+
+    async for message in client.iter_messages(dialog_id):
+        print(message.to_json())
+
+        if media is True:
+            await message.download_media(file="../data/test")
     
 async def main():
+    await scrap_dialog(client, 1001869654432, media=False)
 
-    async for message in client.iter_messages(1001869654432):
-        # You can download media from messages, too!
-        # The method will return the path where the file was saved.
-        await message.download_media(file="../data/test")
 
 client = get_client()
 
